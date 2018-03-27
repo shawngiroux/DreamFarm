1e093e274544e6358cdd8ea243269d8d93ae01c8
allie <littlestcake@gmail.com>
2018-03-27 18:09:55 -0400

    Init DB

--- dreamfarm/__init__.py
+++ dreamfarm/__init__.py
@@ -1,6 +1,9 @@
 import threading
+import os
+import MySQLdb
 from flask import Flask
 from dreamfarm.bot import bot
+from dreamfarm.web.db import DB
 from dreamfarm.web.controllers import web
 from dreamfarm.web.api import api
 
@@ -11,6 +14,8 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(web, url_prefix='/')
     app.register_blueprint(api, url_prefix='/api')
+
+    DB.initialize()
 
     global bot_thread
     bot_thread = threading.Thread(target=bot.run)
